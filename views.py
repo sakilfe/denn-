a165405723fb35e46e25dea35152d479c9f0a73e
ssf! <123976455+sakilfe@users.noreply.github.com>
2023-10-23 11:49:41 -0300

    views.py

--- views.py
+++ views.py
@@ -2,19 +2,19 @@ from cliente import Cliente, NCliente
 
 class View:
     @classmethod
-    def ClienteInserir(cls, nome, email, fone):
+    def cliente_inserir(cls, nome, email, fone):
         cliente = Cliente(0, nome, email, fone)
         NCliente.inserir(cliente)
 
     @classmethod
-    def ClienteListar(cls): 
+    def cliente_listar(cls): 
         return NCliente.listar()
 
     @classmethod
-    def ClienteAtualizar(id, nomenv, emailnv, fonenv):
+    def cliente_atualizar(cls, id, nomenv, emailnv, fonenv):
         cliente = Cliente(id, nomenv, emailnv, fonenv)
         NCliente.atualizar(cliente)
 
     @classmethod
-    def ClienteExcluir(cls, id):
+    def cliente_excluir(cls, id):
         NCliente.excluir(id)
